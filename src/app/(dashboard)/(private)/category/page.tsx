d13c305c045fca1f148914c11a7071bd3166d4b6
topinkilaz <juan.p.t.1234@gmail.com>
2025-11-28 10:25:30 -0400

    CAT-SUB-TITLE

--- src/app/(dashboard)/(private)/category/page.tsx
+++ src/app/(dashboard)/(private)/category/page.tsx
@@ -1,8 +1,29 @@
+// MUI Imports
+import Grid from '@mui/material/Grid2'
+
+// Component Imports
+import { Typography } from '@mui/material'
+
 // Component Imports
 import ProductCategoryTable from '@views/Dashboard/category/ProductCategoryTable'
 
 const eCommerceProductsCategory = () => {
-  return <ProductCategoryTable />
+  return (
+    <Grid container spacing={6}>
+      <Grid size={{ xs: 12 }}>
+        <Typography variant='h4' sx={{ paddingLeft: '20px' }}>
+          Categorías y Subcategorías
+        </Typography>
+       <Typography sx={{ paddingLeft: '20px' }}>
+          Aquí puedes visualizar y gestionar la tabla de <code>categorías</code> y <code>subcategorías</code>.
+        </Typography>
+      </Grid>
+      <Grid size={{ xs: 12 }}>
+        <ProductCategoryTable />
+      </Grid>
+    </Grid>
+  )
 }
+
 
 export default eCommerceProductsCategory
