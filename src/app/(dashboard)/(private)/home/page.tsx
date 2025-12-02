83d59e3674ea469d0a01b4de0c6393bc044e864d
topinkilaz <juan.p.t.1234@gmail.com>
2025-12-02 15:31:14 -0400

    dashboardUpdateF

--- src/app/(dashboard)/(private)/home/page.tsx
+++ src/app/(dashboard)/(private)/home/page.tsx
@@ -10,24 +10,7 @@ import { getInvoiceData } from '@/app/server/actions'
 import LowStock from '@/views/Dashboard/ecommerce/LowStock'
 import BestSellers from '@/views/Dashboard/ecommerce/BestSellers'
 
-/**
- * ! If you need data using an API call, uncomment the below API code, update the `process.env.API_URL` variable in the
- * ! `.env` file found at root of your project and also update the API endpoints like `/apps/invoice` in below example.
- * ! Also, remove the above server action import and the action itself from the `src/app/server/actions.ts` file to clean up unused code
- * ! because we've used the server action for getting our static data.
- */
 
-/* const getInvoiceData = async () => {
-  // Vars
-  const res = await fetch(`${process.env.API_URL}/apps/invoice`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch invoice data')
-  }
-
-  return res.json()
-}
- */
 
 const EcommerceDashboard = async () => {
   // Vars
