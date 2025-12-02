3ce1960333f0c810e30dd945ddff509e56b38200
topinkilaz <juan.p.t.1234@gmail.com>
2025-12-02 15:47:12 -0400

    ErrorBuild

--- src/views/Dashboard/ecommerce/StatisticsCard.tsx
+++ src/views/Dashboard/ecommerce/StatisticsCard.tsx
@@ -36,7 +36,8 @@ const TarjetaEstadisticas = () => {
   const formatNumber = (num: number) => {
     if (num >= 1000000) return `${(num / 1000000).toFixed(1)}M`
     if (num >= 1000) return `${(num / 1000).toFixed(1)}k`
-    return num.toString()
+    
+return num.toString()
   }
 
   const data: DataType[] = [
